19c684d22584a6aace7f005a80ce93c45f228d75
Hai Pham <ngochai285nd@gmail.com>
2023-10-31 17:25:13 +0700

    init project

---
+++ src/main/java/com/haiphamcoder/cqrs/demo/DemoCqrsWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.haiphamcoder.cqrs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCqrsWithSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCqrsWithSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haiphamcoder/cqrs/demo/DemoCqrsWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haiphamcoder.cqrs.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCqrsWithSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
